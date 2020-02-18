223626ddd6eabb5c527df3a334b39715bd7d302a
asaid-0 <60660214+asaid-0@users.noreply.github.com>
2020-02-18 22:04:42 +0200

    Sticky sidebar and flex content core

---
+++ home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<div class="container">
+  <div class="sidebar">sidebar</div>
+  <div class="content">
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+    <div class="product">product</div>
+
+  </div>
+</div>
+
+</body>
+</html>
