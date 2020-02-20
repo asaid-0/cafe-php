05183c1ffc9e875d62ba86b9bae6acc9cb1024be
Maged-Naim <magednaim58@gmail.com>
2020-02-20 10:37:14 +0200

    add products - view products - add category

---
+++ add_category.php
@@ -0,0 +1,73 @@
+<?php
+
+$id = $_POST["id"];
+$name = $_POST["name"];
+
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "cafe";
+
+
+ try {
+    $conn = new PDO('mysql:host='.$dbServername.';dbname='.$dbname, $dbUsername, $dbPassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ }
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+    dd("Connection failed: " . $e->getMessage());
+}
+if(isset($_POST['submit'])){
+
+
+    $query = "INSERT INTO categories ( id , name ) VALUES ( ? , ? );";
+    $stmt = $conn->prepare($query);
+    $stmt->execute([$id,$name]);
+
+    $result = $stmt->rowCount();
+
+    if($result > 0){
+       
+         
+    $query = "SELECT * FROM categories";
+    $data = $conn->query($query);
+
+    echo '<table width="70%" border="1"cellpadding="5" cellspacing="5" style="margin:0 200px;">
+            <tr>
+            <th>ID</th>
+            <th>Name</th>           
+            </tr>';   
+    
+      foreach($data as $row)
+      {
+          echo ' <tr>
+                 <td>'.$row["id"].'</td>                 
+                 <td>'.$row["name"].'</td>
+                  
+               </tr>';
+      }
+      echo '</tabl>';
+        
+    }else{
+        echo'Data rejected!';
+    }
+        
+	}else{
+        ?>
+        <script type="text/javascript">
+            alert('Error while adding category data');
+            window.location.href="add_category.html";
+        </script>
+        <?php
+            
+	}
+
+
+
+
+
+
+
+?>
