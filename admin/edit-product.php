cca7117ad5556d196b7dfd757c9a4b64d3c85e0d
asaid-0 <60660214+asaid-0@users.noreply.github.com>
2020-02-26 04:10:55 -0600

    edit-product.php

---
+++ admin/edit-product.php
@@ -0,0 +1,155 @@
+<?php
+require_once("../database/database.inc.php");
+require_once("../models/products.php");
+
+$product = new Products();
+if(isset($_REQUEST['id']) && !empty($_REQUEST['id'])){
+  $id = $_REQUEST['id'];
+}else{
+  die();
+}
+$cat_name = null;
+$isNewFile = true;
+$p = $product->getProduct($id);
+if(!$p)
+die();
+
+?>
+
+<?php
+ if($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $p['name'] = !empty($_POST['name']) ? $_POST['name'] : $p['name'];
+      $p['price'] = !empty($_POST['price']) ? $_POST['price'] : $p['price'];
+      $p['cat_id'] = !empty($_POST['cat_id']) ? $_POST['cat_id'] : $p['cat_id'];
+      $cat_name = !empty($_POST['cat_name']) ? $_POST['cat_name'] : null;
+      $file_info = $_FILES['file'];
+      $file_name = $file_info['name'];
+      $file_size = $file_info['size'];
+      $file_tmp = $file_info['tmp_name'];
+      $file_type = $file_info['type'];
+      
+      if($p['name'] == "")
+          $errors["name"] = "Name field is required.<br>";
+
+      if(!empty($file_name)) {
+          $extens = explode('.', $file_name);
+          $file_ext = strtolower(end($extens));
+          $extensions = array("jpeg", "jpg", "png");
+      
+          if(in_array($file_ext, $extensions) === false){
+              $errors["file"] = "extension not allowed, please choose a JPEG, JPG or PNG file.";
+          }
+      }
+      else {
+          $isNewFile = false;
+  }
+
+/*******************Data is validated and ready for insertion*******************/
+
+  if(empty($errors)) {
+    if($isNewFile){
+      $pic_name = "../assets/images/"."product_".$name."_".time().".".$file_ext;
+      move_uploaded_file($file_tmp, $pic_name);
+      $p['pic'] = $pic_name;
+    }
+    $product->updateProduct($p, $cat_name);
+  }
+  else {
+    error_log(print_r($errors, TRUE)); 
+    $_SESSION['errors'] = $errors;
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Product</title>
+    <link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link rel="stylesheet" href="../assets/css/add.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
+   
+
+
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="menu-left">
+            <li><a href="#" class="logo">OS Coffee</a></li>
+            <li><a href="#" class="active">Products</a></li>
+            <li><a href="#">Users</a></li>
+            <li><a href="#">Manual Order</a></li>
+            <li><a href="#">Checks</a></li>
+        </ul>
+        <a href="#">
+            <i class="fa fa-user"></i>
+            <span>Admin Dashboard</span>
+        </a>
+    </nav>
+    <section>
+        <div class="add-container">
+            <div class="main-title">Edit Product</div>
+            <div id='form' class='_form'>
+                <form action='' method='POST' enctype="multipart/form-data" name='addProduct'>
+              
+                  <fieldset>
+                    <legend>NAME</legend>
+                    <input type='text' name='name' placeholder='Name' value="<?php echo $p['name'] ?>" />
+                  </fieldset>
+
+                  <fieldset>
+                    <legend>PRICE</legend>
+                    <input type='text' name='price' placeholder='0.00' value="<?php echo $p['price'] ?>" />
+                  </fieldset>
+              
+                  <fieldset id="new_cat">
+                    <legend>NEW CATEGORY</legend>
+                    <input type='text' name='cat_name' placeholder='Category Name' />
+                  </fieldset>
+
+                  <fieldset id="select-menu">
+                    <legend class="category">CATEGORY</legend>
+                    <span onclick="newCategory();" class="new-cat">new category</span>
+                    <label class='input-select'>
+                        <select name='cat_id'>
+                          <option selected='selected' value="<?php echo $p['cat_id'] ?>">- no change -</option>
+                          <option value='1'>Hot Drinks</option>
+                          <option value='2'>Soft Drinks</option>
+                          <option value='3'>Juice</option>
+                        </select>
+                      </label>
+                  </fieldset>
+
+           
+
+
+                  <fieldset>
+                    <legend>PRODUCT PICTURE</legend>
+                    <label for="file" class="file-upload">
+                      <span class="fa fa-cloud-upload"><span> Upload
+                    </label>
+                    <input type='file' name='file' id="file" />
+                   
+                    
+                  </fieldset>
+              
+              
+                  <input type='submit' name='submit' value='Add' />
+                </form>
+              </div>
+        </div>
+    </section>
+    <script type="text/javascript">
+        function newCategory(){
+            // console.log("test");
+            document.getElementById('select-menu').remove();
+            document.getElementById('new_cat').style.display = "block";
+        }
+
+    </script>
+</body>
+</html>
--- models/products.php
+++ models/products.php
@@ -10,22 +10,45 @@ class Products
         $this->conn = $db->getPDO();
     }
 
-
-
+    public function addCategory($category){
+            $stmt = $this->conn->prepare('INSERT INTO `categories` ( `name` ) VALUES (:category)');
+            $stmt->bindParam(":category", $category, PDO::PARAM_STR);
+            $stmt->execute();
+            return $this->conn->lastInsertId();
+    }
     function getProducts(){
         $query = "SELECT * FROM products";
         $data = $this->conn->query($query);
         return $data;
     }
+    function getProduct($id){
+        $query = "SELECT * FROM products where id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    function updateProduct($product, $cat_name){
+        if($cat_name){
+            $product['cat_id'] = $this->addCategory($cat_name);
+        }
+        try{
+        $query = "UPDATE products SET name=:name, cat_id=:cat_id, price=:price, pic=:pic, isAvailable=:isAvailable WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($product);
+        return true;
+        }
+        catch(PDOException $e) {
+            echo 'Update failed!';
+            echo 'Error: ' . $e->getMessage();
+          }
+    }
 
     public function addProduct($category, $name, $price, $pic, $newCat) {
         $cat_id = $category;
         if($newCat){
-            //insert new category
-            $stmt = $this->conn->prepare('INSERT INTO `categories` ( `name` ) VALUES (:category)');
-            $stmt->bindParam(":category", $category, PDO::PARAM_STR);
-            $stmt->execute();
-            $cat_id = $this->conn->lastInsertId();
+            $cat_id = $this->addCategory($category);
         }
         try {
             $query = 'INSERT INTO products (cat_id, name, price, pic) VALUES (:cat_id, :name, :price, :pic)';
