864957d6f98e0599ff96fdde57065246a9a2f9d3
MacrinaAshraf <macrina1997@gmail.com>
2020-02-27 12:26:45 +0200

    final touches

--- admin/admin_make_order.php
+++ admin/admin_make_order.php
@@ -7,19 +7,7 @@ elseif(isset($_SESSION['user-id']) && $_SESSION['admin'] == 0)
 
 require_once("../database/database.inc.php");
 require_once('../models/order.php');
-include "../database/config.php";
 
-$serverName = DB_HOST;
-$username = DB_USER;
-$password = DB_PWD;
-$dbName = DB_NAME;
-try {
-    $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
 
 
 if (isset($_POST['confirm'])) {
@@ -30,7 +18,7 @@ if (isset($_POST['confirm'])) {
     $user_id = $_POST['user'];
 
     if (!empty($drinks) && !empty($quantities) && !empty($room)) {
-        $order = new Order($conn);
+        $order = new Order();
 
         if ($order->create($user_id, $drinks, $quantities, $notes)) {
             $_SESSION['flash'] = 'ordered';
